8d949fcea505f0385a410e73fe15e8f68e047424
Sandeep Rana <rana.sandeep3119@gmail.com>
2021-12-23 18:25:29 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="Traffic-Sign Recognizer", 
+    author="sandeep3119", 
+    packages=find_packages(),
+    license="MIT"
+)
